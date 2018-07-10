99697525792923d2084feae1f176c65a497228c5
Eric Yun <ericjyun@yahoo.com>
2018-07-10 15:16:40 +0000

    Added index.js and made AddToDo functional

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-$(document.ready(function(){
-    $("ul").on("click",input[type="checkbox"],function()
-    {$(this).closest("li").toggleClass("completed-todo")})
-}))
+//$(document).ready(console.log("Perspire salty vinegar"));
+// $(document.ready(function(){
+//     $("ul").on("click",input[type="checkbox"],function(){
+//         $(this).closest("li").toggleClass("completed-todo")})
+
+
+//$(document).ready($("#add-todo-button").click(function(){console.log("This works");}))
+$(document).ready(function(){
+    console.log("Oxidize cells")
+    $("#add-todo-button").click(function(){
+        console.log("Exhale CO2")
+        var description= $("#add-todo-text").val()
+        var time= $("#add-hour").val()
+        console.log(description)
+        $("u1").append("<li><input type='checkbox'/>"+description+" <span class='hour-estimate'>"+ time+" hour(s)</span></li>")
+        console.log(time)
+    })
+})
